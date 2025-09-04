e72588e7ee7675d2913629da02149c1fd0840ba3
dhirac <dhirac@Mac.Home>
2025-09-04 21:40:18 +0100

    added db cred

--- beautyofportugal/settings.py
+++ beautyofportugal/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
