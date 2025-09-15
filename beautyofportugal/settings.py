58c3b485323b8590af5f5911c6352391bd7785e1
dhirac <dhirac@Mac.Home>
2025-09-15 22:03:06 +0100

    chnage smtp port

--- beautyofportugal/settings.py
+++ beautyofportugal/settings.py
@@ -200,7 +200,8 @@ TINYMCE_DEFAULT_CONFIG = {
 # -----------------------------
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465   # change to 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = '93c4da001@smtp-brevo.com'
 EMAIL_HOST_PASSWORD = os.getenv('SENDBLUE_SMTP_API_KEY')
